7e18fd2c84b097c325d5fe2c4fe1485f78089d7f
AryanPatil45 <thearyanpatil0045@gmail.com>
2026-07-27 07:36:28 +0000

    Update PS1 code

---
+++ exp/PS1.py
@@ -0,0 +1,45 @@
+# n=int(input("Enter an integer:"))
+# for i in range(1,n+1):
+#     print(i)
+
+
+# n=int(input("Enter a number: "))
+# for i in range(1,n+1):
+#     if(i%2==0):
+#         print(i)
+
+
+# n=int(input("Enter a number: "))
+# for i in range(1,n+1):
+#     if(i%2==1):
+#         print(i)
+
+
+# n=int(input("Enter a number: "))
+# for i in range(1,n+1):
+#     i=i+i
+#     print(i)
+
+
+
+# n = int(input("Enter the value of n: "))
+
+
+# total_sum = 1.0
+
+
+# current_factorial = 1
+
+
+# for i in range(1, n + 1):
+    
+#     current_factorial = current_factorial * i
+    
+    
+#     total_sum = total_sum + (1 / current_factorial)
+
+# print("The sum of the sequence is:", total_sum)
+
+
+
+
